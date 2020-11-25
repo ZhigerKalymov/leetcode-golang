e14468899b0101adddf7f55d49def75b5ec7a8b1
Zhiger Kalymov <jimbokit.kk@gmail.com>
2020-11-25 12:49:27 +0600

    add test

---
+++ 0009_palindrome_number/main_test.go
@@ -0,0 +1,27 @@
+package main
+
+import "testing"
+
+func Test_isPalindrome(t *testing.T) {
+	type args struct {
+		x int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			"test_121",
+			args{121},
+			true,
+		},
+	}
+		for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isPalindrome(tt.args.x); got != tt.want {
+				t.Errorf("isPalindrome() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
